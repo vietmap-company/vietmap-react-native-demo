4fd0c8c8c07eee01b5208aac3f4e24f76ef00b70
thanhdang198 <dangtrongthanh1998@gmail.com>
2024-02-23 17:35:00 +0700

    [feat:(map)]: Create image overlay demo screen

--- screen/Map/ImageOverLay.tsx
+++ screen/Map/ImageOverLay.tsx
@@ -9,9 +9,9 @@ import Vietmap from '@vietmap/vietmap-gl-react-native';
 import Bubble from '../../common/Bubble';
 import { vietmapStyle } from '../../vietmap_config';
 const styles = {
-    rasterLayer: {rasterOpacity: 0.6},
-    bubble: {bottom: 100},
-  };
+    rasterLayer: { rasterOpacity: 0.6 },
+    bubble: { bottom: 100 },
+};
 
 
 const frames = [radar0, radar1, radar2];
@@ -19,89 +19,89 @@ const frames = [radar0, radar1, radar2];
 
 const coordQuads = [
     [
-      [-80.425, 46.437], // top left
-      [-71.516, 46.437], // top right
-      [-71.516, 37.936], // bottom right
-      [-80.425, 37.936], // bottom left
+        [106.492888, 10.957413], // top left
+        [106.927100, 10.951878], // top right
+        [106.958675, 10.653745], // bottom right
+        [106.444080, 10.654078], // bottom left
     ],
     [
-      [-85.425, 45.437], // top left
-      [-75.516, 45.437], // top right
-      [-75.516, 36.936], // bottom right
-      [-85.425, 36.936], // bottom left
+        [106.512888, 10.977413], // top left
+        [106.947100, 10.971878], // top right
+        [106.978675, 10.673745], // bottom right
+        [106.464080, 10.674078], // bottom left
     ],
-  ];
+];
 
-  class ImageOverlay extends React.Component{
-    state ={
-        radarFrameIndex:0,
+class ImageOverlay extends React.Component {
+    state = {
+        radarFrameIndex: 0,
         coords: coordQuads[0],
-        dynamic:false
+        dynamic: false
     }
 
-    _timeOut : ReturnType<typeof setTimeout>|null   = null
+    _timeOut: ReturnType<typeof setTimeout> | null = null
     componentDidMount(): void {
         this.heartbeat()
     }
 
-    heartbeat(){
-        this._timeOut = setTimeout(()=>{
-            requestAnimationFrame(()=>{
-                let nextFrame = this.state.radarFrameIndex+1;
-                if(nextFrame>1){
-                    nextFrame =0
+    heartbeat() {
+        this._timeOut = setTimeout(() => {
+            requestAnimationFrame(() => {
+                let nextFrame = this.state.radarFrameIndex + 1;
+                if (nextFrame > 1) {
+                    nextFrame = 0
                 }
-                if(this.state.dynamic){
+                if (this.state.dynamic) {
                     this.setState({
-                        radarFrameIndex:nextFrame,
+                        radarFrameIndex: nextFrame,
                         coords: coordQuads[nextFrame]
                     })
-                }else{
-                    this.setState({radarFrameIndex: nextFrame})
+                } else {
+                    this.setState({ radarFrameIndex: nextFrame })
                 }
                 this.heartbeat()
             })
         }, 1000)
     }
     componentWillUnmount(): void {
-        if(this._timeOut){
+        if (this._timeOut) {
             clearTimeout(this._timeOut)
         }
     }
 
     render(): React.ReactNode {
-        
-    const bubbleText = this.state.dynamic
-    ? 'Static coordinates'
-    : 'Dynamic coordinates';
+
+        const bubbleText = this.state.dynamic
+            ? 'Static coordinates'
+            : 'Dynamic coordinates';
 
 
-    return (
-        <View style={{flex:1}}>
-          <Vietmap.MapView 
-            style={{flex:1}}
-            styleURL={vietmapStyle}>
-            <Vietmap.Camera zoomLevel={4} centerCoordinate={[-79, 40]} />
-  
-            <Vietmap.Animated.ImageSource
-              key="d"
-              id="radarSource"
-              coordinates={this.state.coords}
-              url={frames[this.state.radarFrameIndex]}>
-              <Vietmap.RasterLayer id="radarLayer" style={styles.rasterLayer} />
-            </Vietmap.Animated.ImageSource>
-          </Vietmap.MapView>
-          <Bubble
-            onPress={() => {
-              this.setState({dynamic: !this.state.dynamic});
-            }}
-            style={styles.bubble}>
-            <Text>{bubbleText}</Text>
-          </Bubble>
-        </View>
-      );
+        return (
+            <View style={{ flex: 1 }}>
+                <Vietmap.MapView
+                    style={{ flex: 1 }}
+                    styleURL={vietmapStyle}>
+                    <Vietmap.Camera zoomLevel={9} centerCoordinate={[106.657948, 10.798708]} />
+
+                    <Vietmap.Animated.ImageSource
+                        key="d"
+                        id="radarSource"
+                        coordinates={this.state.coords}
+                        url={frames[this.state.radarFrameIndex]}>
+                        <Vietmap.RasterLayer id="radarLayer" style={styles.rasterLayer} />
+                    </Vietmap.Animated.ImageSource>
+                </Vietmap.MapView>
+                <Bubble
+                    onPress={() => {
+                        this.setState({ dynamic: !this.state.dynamic });
+                    }}
+                    style={styles.bubble}>
+                    <Text>{bubbleText}</Text>
+                </Bubble>
+            </View>
+        );
     }
-  }
+}
 
 
 export default ImageOverlay;
